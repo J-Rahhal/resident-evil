79c6954b445c2aa5b46653d7504e4facf8c8669d
Joanna Rahhal <joanna.h.rahhal@gmail.com>
2025-04-18 13:50:30 +0300

    Creating welcome screen text animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Hero from "./components/Hero";
 import Loader from "./components/Loader";
 import Welcome from "./components/Welcome";
 import "./index.css";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
   return (
     <div className="h-screen w-full">
-      {loading && <Loader onComplete={() => setLoading(false)} />}
-      {!loading && <Hero />}
+      {/* {loading && <Loader onComplete={() => setLoading(false)} />}
+      {!loading && <Hero />} */}
+      <Hero />
       <Welcome />
     </div>
   );
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,61 @@
-import React from "react";
+import gsap from "gsap";
+import React, { useEffect, useRef } from "react";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Welcome = () => {
+  const backgroundRef = useRef(null);
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const spans = textRef.current.querySelectorAll("span");
+
+    gsap.to(backgroundRef.current, {
+      backgroundColor: "black",
+      ease: "none",
+      scrollTrigger: {
+        trigger: backgroundRef.current,
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+      },
+    });
+
+    gsap.set(spans, { opacity: 0, y: 20, filter: "blur(4px)" });
+
+    gsap.to(spans, {
+      opacity: 1,
+      y: 0,
+      filter: "blur(0px)",
+      color: "red",
+      duration: 14,
+      ease: "power2.inOut",
+      stagger: 0.8,
+      scrollTrigger: {
+        trigger: textRef.current,
+        start: "top 100%",
+        scrub: true,
+      },
+    });
+  }, []);
+
   return (
-    <div className="relative w-full h-[100dvh] overflow-x-hidden pt-30 ">
-      <div className="flex justify-center">
-        <h2>Hello</h2>
+    <div
+      className="relative w-full min-h-screen overflow-x-hidden pt-30"
+      ref={backgroundRef}
+    >
+      <div className="h-full flex justify-center">
+        <h2
+          className="text-white text-2xl text-center font-secondary px-4 md:text-4xl md:px-6 lg:text-6xl lg:w-[50%]"
+          ref={textRef}
+        >
+          <span>Forget</span> <span>everything</span> <span>you</span>{" "}
+          <span>know </span> <span>about</span>{" "}
+          <span className="text-red-500">R</span>
+          <span>esident</span> <span className="text-red-500">E</span>
+          <span>vil</span>
+        </h2>
       </div>
     </div>
   );
