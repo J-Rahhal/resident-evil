6098b096dc59d35e2ba6915f48f02597ad4d12b5
Joanna Rahhal <joanna.h.rahhal@gmail.com>
2025-04-18 13:51:33 +0300

    uncommenting components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "./components/Hero";
 import Loader from "./components/Loader";
 import Welcome from "./components/Welcome";
 import "./index.css";
 
 const App = () => {
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   return (
     <div className="h-screen w-full">
-      {/* {loading && <Loader onComplete={() => setLoading(false)} />}
-      {!loading && <Hero />} */}
+      {loading && <Loader onComplete={() => setLoading(false)} />}
+      {!loading && <Hero />}
       <Hero />
       <Welcome />
     </div>
